b1f808221123c8e47c6ee5f1f6b05afd3158a573
alexgonzalezz1 <148534626+alexgonzalezz1@users.noreply.github.com>
2023-10-20 14:12:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parents New healthy Diner')
 
-streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo duro de gallinas camperas')
+streamlit.header('🥣Menú de desayuno')
+streamlit.text('🥗Omega 3 y avena con arándanos')
+streamlit.text('🐔Batido de col rizada, espinacas y rúcula')
+streamlit.text('🥑🍞Huevo duro de gallinas camperas')
 
