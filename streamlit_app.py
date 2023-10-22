38737ba0331e0cbe084eb8d12340ad6a8c5e9c1a
alexgonzalezz1 <148534626+alexgonzalezz1@users.noreply.github.com>
2023-10-22 20:03:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,10 @@ streamlit.dataframe(fruits_to_show)
 # Let's put a pick list here so they can pick the fruit they want to include 
 
 # Display the table on the page.
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json)
