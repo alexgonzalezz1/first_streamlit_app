ccac289fdcc91cce15120c1d98702e8f8d4833eb
alexgonzalezz1 <148534626+alexgonzalezz1@users.noreply.github.com>
2023-10-23 17:41:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,11 +58,18 @@ if streamlit.button('Get fruit load list'):
         streamlit.error("Error connecting to Snowflake or retrieving data.")
         streamlit.error(str(e))
 
-streamlit.stop()
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('¿Qué fruta le gustaría añadir?', 'Kiwi')
-streamlit.write('El usuario ingresó ', fruit_choice)
+def insert_row_snowflake(new_fruit):
+     with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thansk for adding" + new fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
+if streamlit.button('Add a Fruit to the List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        streamlit.text(back_from_function)
 
 if streamlit.button('Add fruit to Snowflake'):
     try:
