c26c29a64d1c2cef70109c140ed51630878f12e0
alexgonzalezz1 <148534626+alexgonzalezz1@users.noreply.github.com>
2023-10-23 18:07:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,4 +81,10 @@ if streamlit.button('Add fruit to Snowflake'):
         streamlit.error(str(e))
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.dataframe(my_data_rows)
+
 
