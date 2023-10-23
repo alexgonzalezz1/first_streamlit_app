db44c207cabe9e7153ddfcad1c7aea4ffebebe02
alexgonzalezz1 <148534626+alexgonzalezz1@users.noreply.github.com>
2023-10-23 17:51:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,3 +79,10 @@ if streamlit.button('Add fruit to Snowflake'):
         streamlit.error("Error adding data to Snowflake.")
         streamlit.error(str(e))
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
